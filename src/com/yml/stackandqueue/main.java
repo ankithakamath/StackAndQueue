a0cc673d0a4a301c0f4b4ed838719e5f50ea38da
Ankitha Kamath <ankithakamath@196AMBBLR-Ankitha-Kamath.local>
2021-09-14 22:01:51 +0530

    Welcome message added

---
+++ src/com/yml/stackandqueue/main.java
@@ -0,0 +1,10 @@
+package com.yml.stackandqueue;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Welcome to Stack and Queue Practice");
+	}
+
+}
